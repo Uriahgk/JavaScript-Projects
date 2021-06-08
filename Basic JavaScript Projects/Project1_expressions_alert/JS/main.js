a0706220f4e0dafeec02d9382ad153c34a931867
Uriah Kumangai <84421608+Uriahgk@users.noreply.github.com>
2021-06-08 16:54:18 -0700

    Added two files
    Added index.html and main.js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,26 @@
+document.write ("Hello, World!");
+
+var U = "Uriah";
+
+document.write(U);
+
+window.alert(U);
+
+document.write ("As I ran down the stairs, Bob yelled, \"Watch out! Don\'t trip!\"");
+
+document.write ("John ran down the stairs to the door," + " opened it, and ran outside.");
+
+var K = "Uriah" + " Kumangai";
+document.write(K);
+
+var personA = "Bob", personB = "Joe", personC = "Tom", personD = "Jerry";
+var personA = personA.fontcolor("blue");
+var personB = personB.fontcolor("red");
+var personC = personC.fontcolor("yellow");
+var personD = personD.fontcolor("green");
+document.write(personA);
+document.write(personB);
+document.write(personC);
+document.write(personD);
+
+document.write(4+4);
