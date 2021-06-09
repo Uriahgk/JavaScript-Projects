380403d668e75b7ca1e9f75a67c04f3e63fe95ec
Uriah Kumangai <84421608+Uriahgk@users.noreply.github.com>
2021-06-09 09:51:04 -0700

    Updated main.js
    Added comments explaining code.

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,26 +1,26 @@
-document.write ("Hello, World!");
+document.write ("Hello, World!"); //writes the string on the page
 
-var U = "Uriah";
+var U = "Uriah"; //gave the variable U the value of Uriah
 
-document.write(U);
+document.write(U); //this will write Uriah on the page
 
-window.alert(U);
+window.alert(U); //this makes a pop up window that displays Uriah
 
-document.write ("As I ran down the stairs, Bob yelled, \"Watch out! Don\'t trip!\"");
+document.write ("As I ran down the stairs, Bob yelled, \"Watch out! Don\'t trip!\""); //this is allowing me to write quotations on the page
 
-document.write ("John ran down the stairs to the door," + " opened it, and ran outside.");
+document.write ("John ran down the stairs to the door," + " opened it, and ran outside."); //this is concatenating two strings
 
-var K = "Uriah" + " Kumangai";
-document.write(K);
+var K = "Uriah" + " Kumangai"; // this is concatenating two values and giving them to the variable K
+document.write(K); //this writes out the var K onto the page
 
-var personA = "Bob", personB = "Joe", personC = "Tom", personD = "Jerry";
+var personA = "Bob", personB = "Joe", personC = "Tom", personD = "Jerry"; //this is assigning multiple variables a value all in one line
 var personA = personA.fontcolor("blue");
 var personB = personB.fontcolor("red");
 var personC = personC.fontcolor("yellow");
-var personD = personD.fontcolor("green");
+var personD = personD.fontcolor("green"); // this is changing the font color of all those variables 
 document.write(personA);
 document.write(personB);
 document.write(personC);
-document.write(personD);
+document.write(personD); // these are printing all said variables onto the page
 
-document.write(4+4);
+document.write(4+4); // this is a statement with an expression. it's adding 4 and 4 and will show 8 on the page.
